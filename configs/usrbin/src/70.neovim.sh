529fa27dc46b4e89f2f0f8ebd2fdbc3f9c5ccc61
Sean Enck <sean@ttypty.com>
2025-01-18 08:50:44 -0500

    this should use the same protocol

--- configs/usrbin/src/70.neovim.sh
+++ configs/usrbin/src/70.neovim.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -e
 VERSION=0.10.3
 FILE="$PKGS_WD/$VERSION.neovim.tar.gz"
+HASH="5bd9083"
 
-[ ! -e "$FILE" ] && scp "release.ttypty.com:~/Resources/v$VERSION-nvim-linux64.tar.gz" "$FILE"
+download-and-check \
+  -u "https://voidedtech.com/0.10.3.neovim.tar.gz" \
+  -f "$FILE" \
+  -h "$HASH"
 
 tar xf "$FILE" -C "$PKGS_LIB"
 (cd "$PKGS_BIN" && ln -sf "../$PKGS_LIB_DIR/nvim-linux64/bin/nvim" nvim)
