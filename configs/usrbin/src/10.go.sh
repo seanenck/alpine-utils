81b37f2e07ded884386d6f2ee28e3f337c0ad9e5
Sean Enck <sean@ttypty.com>
2025-01-19 09:05:44 -0500

    pass a project as an alternative to detection

--- configs/usrbin/src/10.go.sh
+++ configs/usrbin/src/10.go.sh
@@ -6,7 +6,8 @@ FILE="$PKGS_WD/$VERSION.go.tar.gz"
 download-and-check \
   -u "https://go.dev/dl/go$VERSION.linux-$PKGS_ALTARCH.tar.gz" \
   -f "$FILE" \
-  -h "$HASH"
+  -h "$HASH" \
+  -p "golang/go"
 
 tar xf "$FILE" -C "$PKGS_LIB"
 (cd "$PKGS_BIN" && ln -sf "../$PKGS_LIB_DIR/go/bin/go" go)
--- configs/usrbin/src/70.neovim.sh
+++ configs/usrbin/src/70.neovim.sh
@@ -6,7 +6,8 @@ HASH="5bd9083"
 download-and-check \
   -u "https://voidedtech.com/0.10.3.neovim.tar.gz" \
   -f "$FILE" \
-  -h "$HASH"
+  -h "$HASH" \
+  -p "neovim/neovim"
 
 tar xf "$FILE" -C "$PKGS_LIB"
 (cd "$PKGS_BIN" && ln -sf "../$PKGS_LIB_DIR/nvim-linux64/bin/nvim" nvim)
