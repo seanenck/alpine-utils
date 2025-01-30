8a2ecf290a2f2539033516d274f731cfb1e853d0
Sean Enck <sean@ttypty.com>
2025-01-29 19:23:42 -0500

    neovim update

--- configs/usrbin/src/70.neovim.sh
+++ configs/usrbin/src/70.neovim.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
-VERSION=0.10.3
+VERSION=0.10.4
 FILE="$PKGS_WD/$VERSION.neovim.tar.gz"
-HASH="5bd9083"
+HASH="f411777"
 
 download-and-check \
-  -u "https://voidedtech.com/0.10.3.neovim.tar.gz" \
+  -u "https://voidedtech.com/$VERSION-nvim-linux-$PKGS_ALTARCH.tar.gz" \
   -f "$FILE" \
   -h "$HASH" \
   -p "neovim/neovim"
 
 tar xf "$FILE" -C "$PKGS_LIB"
-(cd "$PKGS_BIN" && ln -sf "../$PKGS_LIB_DIR/nvim-linux64/bin/nvim" nvim)
+(cd "$PKGS_BIN" && ln -sf "../$PKGS_LIB_DIR/nvim-linux-$PKGS_ALTARCH/bin/nvim" nvim)
